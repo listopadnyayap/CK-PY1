d675d705546583942a75e1da17e69b7bdc26f495
listopadnyayap <115485776+listopadnyayap@users.noreply.github.com>
2022-11-01 01:35:28 +0700

    Update task_2.py
    Добавлена вторая функция для подсчёта процентного отношения и исправлены ошибки согласно замечаниям

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -1,9 +1,14 @@
+symbol_dict=dict()
 def get_count_char(str_):
-    symbol_dict=dict()
+    str_ = ''.join(main_str.lower().split(' '))
     for symbol in str_: #Перебор всех символов в строке
         if symbol.isalpha() and symbol not in symbol_dict: #Условие, при котором символ является алфавитным и ещё не включен в словарь
-            if str_.count(symbol)>=1:
-                symbol_dict[symbol]=str_.count(symbol) #Добавление новых элементов в словарь
+            symbol_dict[symbol]=str_.count(symbol) #Добавление новых элементов в словарь
+    return symbol_dict
+def get_percent(symbol_dict):
+    sum_ = sum(symbol_dict.values())
+    for i in symbol_dict:
+        symbol_dict[i] = round((symbol_dict[i] / sum_) * 100, 3)
     return symbol_dict
 
 main_str = """
@@ -11,13 +16,4 @@ main_str = """
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
-str_=''.join(main_str.lower().split(' '))
-print(get_count_char(str_))
-
-
-
-
-
-
-
-
+print(get_count_char(main_str))
